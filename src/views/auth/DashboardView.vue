bc160681607ed241a7132294da8823611c90b7e1
lance <lancebrangan@gmail.com>
2025-04-21 22:31:44 +0800

    fix

--- src/views/auth/DashboardView.vue
+++ src/views/auth/DashboardView.vue
@@ -14,20 +14,11 @@ function goToAppointments() {
 
 <template>
   <v-app>
-    <v-main class="fill-height" style="background-color: #ffd54f;">
+    <v-main class="fill-height" style="background-color: #ffd54f">
       <v-container fluid class="fill-height">
-        <v-row
-          align="center"
-          justify="center"
-          class="text-center fill-height"
-        >
+        <v-row align="center" justify="center" class="text-center fill-height">
           <v-col cols="12" md="6">
-            <img
-              src="@/assets/background.jpg"
-              alt="Logo"
-              class="mb-6"
-              style="max-width: 300px"
-            />
+            <img src="@/assets/background.jpg" alt="Logo" class="mb-6" style="max-width: 300px" />
 
             <v-btn
               color="orange"
@@ -52,20 +43,22 @@ function goToAppointments() {
             </v-btn>
           </v-col>
 
-
           <!-- Profile Icon top-right -->
-<v-col cols="12" class="d-flex justify-end px-6"  style="position: absolute; top: 0; right: 0;">
-  <v-btn
-  color="yellow darken-2"
-  icon
-  class="ma-4 elevation-4"
-  style="border-radius: 50%;"
-  @click="router.push('/profile')"
->
-    <v-icon size="32" >mdi-account-circle</v-icon>
-  </v-btn>
-</v-col>
-
+          <v-col
+            cols="12"
+            class="d-flex justify-end px-6"
+            style="position: absolute; top: 0; right: 0"
+          >
+            <v-btn
+              color="yellow darken-2"
+              icon
+              class="ma-4 elevation-4"
+              style="border-radius: 50%"
+              @click="router.push('/profiles')"
+            >
+              <v-icon size="32">mdi-account-circle</v-icon>
+            </v-btn>
+          </v-col>
         </v-row>
       </v-container>
     </v-main>
