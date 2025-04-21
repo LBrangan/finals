fa9f3f8a9f173005c0663598a04ec7d88c1d11f4
Angel <angelmarohomar@gmail.com>
2025-04-21 20:38:51 +0800

    booking

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import DashboardView from '@/views/auth/DashboardView.vue'
+import ProfilesView from '@/views/auth/ProfilesView.vue'
+import BookingView from '@/views/auth/BookingView.vue'
 
 
 const router = createRouter({
@@ -23,6 +25,18 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView
+    },
+
+    {
+      path: '/profiles',
+      name: 'profiles',
+      component: ProfilesView
+    },
+
+    {
+      path: '/booking',
+      name: 'booking',
+      component: BookingView
     }
 
   ]
