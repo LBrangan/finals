7468002070138a3a95eb6c879519e8d3e3cfd1e8
lance <lancebrangan@gmail.com>
2025-04-25 20:16:45 +0800

    AppLayout Updates

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,8 +1,8 @@
 <script setup>
+import AppLayout from '@/components/layout/AppLayout.vue'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
-const theme = ref('light')
 const form = ref({
   firstName: '',
   lastName: '',
@@ -51,152 +51,127 @@ const handleSubmit = async () => {
     loading.value = false
   }
 }
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <!-- Floating Dark Mode Toggle -->
-      <v-btn
-        icon
-        :color="theme === 'light' ? 'yellow darken-3' : 'blue-grey darken-3'"
-        @click="onClick"
-        class="floating-toggle"
-      >
-        <v-icon>{{ theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
-      </v-btn>
+  <AppLayout>
+    <template #content>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="11" md="10" lg="9">
+          <v-row>
+            <!-- Logo Section - Hide on small screens -->
+            <v-col cols="12" md="6" class="d-none d-md-flex align-center justify-center pa-8">
+              <img src="@/assets/background.jpg" alt="Logo" class="responsive-img" />
+            </v-col>
 
-      <v-main>
-        <v-container fluid class="bg-amber-lighten-2 fill-height">
-          <v-row align="center" justify="center">
-            <v-col cols="12" sm="11" md="10" lg="9">
-              <v-row>
-                <!-- Logo Section - Hide on small screens -->
-                <v-col cols="12" md="6" class="d-none d-md-flex align-center justify-center pa-8">
-                  <img src="@/assets/background.jpg" alt="Logo" class="responsive-img" />
+            <!-- Form Section -->
+            <v-col cols="12" sm="10" md="6" class="mx-auto">
+              <v-card class="pa-4 pa-sm-6" elevation="2" rounded="lg">
+                <v-row justify="center">
+                  <v-col cols="12" class="text-center">
+                    <h1 class="mt-2 mb-4 font-weight-bold">Create Account</h1>
+                    <h2 class="mt-2 mb-4 text-subtitle-2">Create account to get started</h2>
+                  </v-col>
+                </v-row>
+
+                <v-row dense>
+                  <v-col cols="6">
+                    <v-text-field
+                      v-model="form.firstName"
+                      label="First name"
+                      variant="outlined"
+                      prepend-inner-icon="mdi-rename"
+                      :error-messages="errors.firstName"
+                      @input="errors.firstName = ''"
+                    />
+                  </v-col>
+
+                  <v-col cols="6">
+                    <v-text-field
+                      v-model="form.lastName"
+                      label="Last name"
+                      variant="outlined"
+                      prepend-inner-icon="mdi-rename"
+                      :error-messages="errors.lastName"
+                      @input="errors.lastName = ''"
+                    />
+                  </v-col>
+                </v-row>
+
+                <v-row dense>
+                  <v-col>
+                    <v-text-field
+                      v-model="form.program"
+                      label="Program"
+                      variant="outlined"
+                      class="mb-3 mt-1"
+                      prepend-inner-icon="mdi-rename"
+                      :error-messages="errors.program"
+                      @input="errors.program = ''"
+                    />
+                  </v-col>
+                </v-row>
+
+                <v-text-field
+                  v-model="form.email"
+                  label="Email address"
+                  variant="outlined"
+                  class="mb-3"
+                  prepend-inner-icon="mdi-email"
+                  :error-messages="errors.email"
+                  @input="errors.email = ''"
+                />
+
+                <v-text-field
+                  v-model="form.password"
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  class="mb-3"
+                  prepend-inner-icon="mdi-lock"
+                  :error-messages="errors.password"
+                  @input="errors.password = ''"
+                />
+
+                <v-text-field
+                  v-model="form.confirmPassword"
+                  label="Confirm password"
+                  variant="outlined"
+                  type="password"
+                  class="mb-3"
+                  prepend-inner-icon="mdi-lock"
+                  :error-messages="errors.confirmPassword"
+                  @input="errors.confirmPassword = ''"
+                />
+
+                <v-btn
+                  color="orange"
+                  class="white--text mb-3 font-weight-bold"
+                  block
+                  :loading="loading"
+                  @click="handleSubmit"
+                >
+                  Sign Up
+                </v-btn>
+
+                <v-col class="text-center">
+                  Already have an account?
+                  <RouterLink
+                    to="/"
+                    class="text-decoration-none font-weight-bold text-orange text--darken-3"
+                    >Sign In</RouterLink
+                  >
                 </v-col>
-
-                <!-- Form Section -->
-                <v-col cols="12" sm="10" md="6" class="mx-auto">
-                  <v-card class="pa-4 pa-sm-6" elevation="2" rounded="lg">
-                    <v-row justify="center">
-                      <v-col cols="12" class="text-center">
-                        <h1 class="mt-2 mb-4 font-weight-bold">Create Account</h1>
-                        <h2 class="mt-2 mb-4 text-subtitle-2">Create account to get started</h2>
-                      </v-col>
-                    </v-row>
-
-                    <v-row dense>
-                      <v-col cols="6">
-                        <v-text-field
-                          v-model="form.firstName"
-                          label="First name"
-                          variant="outlined"
-                          prepend-inner-icon="mdi-rename"
-                          :error-messages="errors.firstName"
-                          @input="errors.firstName = ''"
-                        />
-                      </v-col>
-
-                      <v-col cols="6">
-                        <v-text-field
-                          v-model="form.lastName"
-                          label="Last name"
-                          variant="outlined"
-                          prepend-inner-icon="mdi-rename"
-                          :error-messages="errors.lastName"
-                          @input="errors.lastName = ''"
-                        />
-                      </v-col>
-                    </v-row>
-
-                    <v-row dense>
-                      <v-col>
-                        <v-text-field
-                          v-model="form.program"
-                          label="Program"
-                          variant="outlined"
-                          class="mb-3 mt-1"
-                          prepend-inner-icon="mdi-rename"
-                          :error-messages="errors.program"
-                          @input="errors.program = ''"
-                        />
-                      </v-col>
-                    </v-row>
-
-                    <v-text-field
-                      v-model="form.email"
-                      label="Email address"
-                      variant="outlined"
-                      class="mb-3"
-                      prepend-inner-icon="mdi-email"
-                      :error-messages="errors.email"
-                      @input="errors.email = ''"
-                    />
-
-                    <v-text-field
-                      v-model="form.password"
-                      label="Password"
-                      variant="outlined"
-                      type="password"
-                      class="mb-3"
-                      prepend-inner-icon="mdi-lock"
-                      :error-messages="errors.password"
-                      @input="errors.password = ''"
-                    />
-
-                    <v-text-field
-                      v-model="form.confirmPassword"
-                      label="Confirm password"
-                      variant="outlined"
-                      type="password"
-                      class="mb-3"
-                      prepend-inner-icon="mdi-lock"
-                      :error-messages="errors.confirmPassword"
-                      @input="errors.confirmPassword = ''"
-                    />
-
-                    <v-btn
-                      color="orange"
-                      class="white--text mb-3 font-weight-bold"
-                      block
-                      :loading="loading"
-                      @click="handleSubmit"
-                    >
-                      Sign Up
-                    </v-btn>
-
-                    <v-col class="text-center">
-                      Already have an account?
-                      <RouterLink
-                        to="/"
-                        class="text-decoration-none font-weight-bold text-orange text--darken-3"
-                        >Sign In</RouterLink
-                      >
-                    </v-col>
-                  </v-card>
-                </v-col>
-              </v-row>
+              </v-card>
             </v-col>
           </v-row>
-        </v-container>
-      </v-main>
-    </v-app>
-  </v-responsive>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
 
 <style scoped>
-.floating-toggle {
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  z-index: 1000;
-}
-
 .bg-amber-lighten-2 {
   background-color: #ffd54f;
 }
